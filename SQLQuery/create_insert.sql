995c4c06d0b36b86e17eaad1ff5942f5add5e055
ajh831 <gnstm831@naver.com>
2024-04-03 21:03:55 +0900

    sql

---
+++ SQLQuery/create_insert.sql
@@ -0,0 +1,61 @@
+use springbasic;
+
+show tables;
+
+CREATE TABLE registerTB (
+  idx INT(11) NOT NULL AUTO_INCREMENT,
+  id VARCHAR(8) NOT NULL UNIQUE,
+  pwd VARCHAR(12) NOT NULL,
+  name VARCHAR(10) NOT NULL,
+  birth DATE,
+  address  VARCHAR(255),
+  CONSTRAINT testTable_PK PRIMARY KEY(id)
+);
+
+CREATE TABLE registerTB (
+  idx INT(11) NOT NULL AUTO_INCREMENT,
+  id VARCHAR(8) NOT NULL,
+  pwd VARCHAR(12) NOT NULL,
+  name VARCHAR(10) NOT NULL,
+  birth DATE,
+  address VARCHAR(255),
+  PRIMARY KEY (id),
+  UNIQUE KEY idx (idx)
+);
+
+show tables;
+
+INSERT INTO REGISTERTB(ID, PWD, NAME) VALUES ('ASD2', 'AFDFS', 'DDD');
+
+SELECT * FROM REGISTERTB;
+
+SELECT COUNT(*) FROM REGISTERTB;
+
+DROP TABLE REGISTERTB;
+
+COMMIT;
+
+CREATE TABLE registerTB (
+  id VARCHAR(8),
+  pwd VARCHAR(12) NOT NULL,
+  name VARCHAR(10) NOT NULL,
+  birth DATE,
+  address VARCHAR(255),
+  reg_date Date,
+  CONSTRAINT testTable_PK PRIMARY KEY (id)
+);
+
+select * from registerTB;
+
+-- board : bno, title, content, writer, view_cnt, comment_cnt, reg_date, up_date
+-- insert select
+insert into board(title, content, writer, view_cnt, comment_cnt, reg_date, up_date)
+select title, content, writer, view_cnt, comment_cnt, reg_date, up_date from board;
+-- 위에 계속하면 계속 삽입됨 -> 이렇게 데이터 넣고 페이징 테스트하면 됨
+
+-- insert into board(title, content, writer, view_cnt, comment_cnt, reg_date, up_date)
+-- values("title1", "content1", "writer1", 0, 0 ,now(), now());
+
+show variables like 'c%';
+
+INSERT INTO REGISTERTB() VALUES('asdf', '1234', '홍길동', '1999-01-01', '서울특별시 강남구 테헤란로', now());
---
+++ SQLQuery/select_db.sql
@@ -0,0 +1,14 @@
+show databases;
+
+use springbasic;
+
+show tables;
+
+SELECT * FROM Information_schema.tables 
+WHERE table_schema = 'springbasic';
+
+select now();
+
+show tables;
+
+select * from board;
