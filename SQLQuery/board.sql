615cc6cc0444d2c4ca428f87e39eb79e61054f6a
ajh831 <gnstm831@naver.com>
2024-04-05 19:30:08 +0900

    boad sql query

---
+++ SQLQuery/board.sql
@@ -0,0 +1,33 @@
+select * from board;
+
+DROP TABLE BOARD;
+
+CREATE TABLE board (
+  bno INT(11) NOT NULL AUTO_INCREMENT,	-- 게시글 번호
+  title VARCHAR(255) NOT NULL, -- 게시글 제목
+  content VARCHAR(255) NOT NULL, -- 게시글 내용
+  writer VARCHAR(45) NOT NULL, -- 게시글 작성자
+  view_cnt INT(11) NOT NULL, -- 조회수
+  comment_cnt INT(11) NOT NULL, -- 댓글 수
+  reg_date DATE NOT NULL, -- 작성일
+  up_date DATE, -- 수정일
+  CONSTRAINT board PRIMARY KEY (bno)
+);
+
+SELECT * FROM BOARD;
+
+insert into board(title, content, writer, view_cnt, comment_cnt, reg_date, up_date)
+values("title1", "content1", "asdf", 0, 0 ,now(), now());
+
+-- board : bno, title, content, writer, view_cnt, comment_cnt, reg_date, up_date
+-- insert select
+insert into board(title, content, writer, view_cnt, comment_cnt, reg_date, up_date)
+select title, content, writer, view_cnt, comment_cnt, reg_date, up_date from board;
+-- 위에 계속하면 계속 삽입됨 -> 이렇게 데이터 넣고 페이징 테스트하면 됨
+
+insert into board(title, content, writer, view_cnt, comment_cnt, reg_date, up_date)
+values("title1", "content1", "writer1", 0, 0 ,now(), now());
+
+select count(*) from board;
+
+SELECT now();
